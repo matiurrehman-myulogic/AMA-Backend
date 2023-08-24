975e39cd4dc8fffef04458ef982d840dcc675817
Hisam <hisamuddin@myulogic.com>
2023-08-24 17:09:57 +0530

    Added eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true, 
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['node_modules/', 'android/', 'ios/'],
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/no-unstable-nested-components': ['warn', {allowAsProps: true}],
+    'operator-linebreak': 'off',
+    'padded-blocks': 'off',
+    'quote-props': 'off',
+    radix: 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-floating-promises': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Ask me Anything')
-    .setDescription('A set of all the API`s required for  AskMeAnything application')
+    .setDescription(
+      'A set of all the API`s required for  AskMeAnything application',
+    )
     .setVersion('1.0')
-    // .addBearerAuth(
-    //   {
-    //     type: 'http',
-    //     scheme: 'bearer',
-    //     bearerFormat: 'JWT',
-    //     in: 'header',
-    //   },
-    //   'JWT',
-    // )
+
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
