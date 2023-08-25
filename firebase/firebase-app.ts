272833a6e22e162626615502d9ea1f1db839d988
riyaShahmyulogic <124568989+riyaShahmyulogic@users.noreply.github.com>
2023-08-25 13:00:25 +0530

    initialized firestore

---
+++ firebase/firebase-app.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import * as firebase from 'firebase-admin';
+import firebaseConfig from './firebase-config';
+
+@Injectable()
+export class FirebaseApp {
+  private firebaseApp: firebase.app.App;
+
+  constructor() {
+    this.firebaseApp = firebase.initializeApp({
+      credential: firebase.credential.cert({ ...firebaseConfig }),
+    });
+  }
+
+  getAuth = (): firebase.auth.Auth => {
+    return this.firebaseApp.auth();
+  };
+
+  firestore = (): firebase.firestore.Firestore => {
+    return this.firebaseApp.firestore();
+  };
+
+  sendPushNotification = async (
+    deviceToken: string,
+    title: string,
+    body: string,
+  ): Promise<void> => {
+    try {
+      const message: firebase.messaging.Message = {
+        notification: {
+          title,
+          body,
+        },
+        token: deviceToken,
+      };
+
+      await this.firebaseApp.messaging().send(message);
+    } catch (error) {
+      console.error('Error sending push notification:', error);
+    }
+  };
+}
---
+++ firebase/firebase-config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    //   projectId: process.env.FIREBASE_PROJECT_ID,
+    //   privateKey: process.env.FIREBASE_PRIVATE_KEY
+      clientEmail: 'firebase-adminsdk-hu2r5@askmeanything-2160b.iam.gserviceaccount.com',
+      project_id: "askmeanything-2160b",
+      private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC271F1EFrvbLHg\n4DJuqnhytwoCxG2j7Fays8NVnxntowoax5Orvxmr5CHFrj3j9aQ99/9eIwsazMiP\nmYUZ9qdYJytGvmLHmXNuDxk56QNsSQ13szCA9tpgmKkvX8Tz4heOUGnOgD/NaPw6\ncCPuhzG81nAhVkHhaT76efL9Et1BZLKSGxvkwo/ZnTSkwe9U6vA4t+BkPHaCpuEe\nqRP6hVV5wcmoeRsP3YEFj56SUywqRyS+3AKIxL5bycDMES5fY6mKuY4mSEvYCyyT\ncSBk3tBIMSYdwAl1GkGW1GONvP1NQSxz0+neQdNBeV0FQ4U+T1890CtaqQuecRoQ\n1DDPHBjrAgMBAAECggEADzfhgueVaQ2VzqtYBrTPyFtJzekhZd2naL5jpZ/SvF4v\n3QJ1WnhPXmlgFd3k8C3jN/SFngM0TUpz9N5EoGBQk5jOlNgJx6kQXTuMUbD8w+lf\n4XFau5/NkvOgP3ysGc3ZP9GYu8EXjbMKKKJjmthMXvVH5r4SRPpIbvXuLPeAyIu/\n3UmMnbb3+57kGEN2yh6Ccp28epap+tsEG81jGUF+Z1st+GGD7irKcovVjbntmwF/\n0YkOySqeaCsBQYQo6r4nAGLtuDk+KPl33dTeAmkafQlpeuHirukTd3wkAmp2yL0a\nzWHa5LiTB+EZkPxZIwxMd7EP6/KprMFcllsZXoBE4QKBgQDrVupYzZVz8ZikEYUQ\nGEXdz0TAdqrYfAo6vsjug+mQgdhwZeN5ctixkpMqHFpK+F+zCID2hYHhFRalaV+q\n0RoPPzvCNBe8P69DJxzXP1QFdGCLdm8I5qqsRopZ4iZBJf9oxD6DTkK8qrqyf7o3\nn5Gh5CAi1Sbm/APhtf1PoWv6hwKBgQDG/qVPPVLIBD5tTfPap6RLRNDjz16w7KVW\nmygiJXEmaFeJjPxa0dtF5sO6cd1NJezyOioyyYjA+OAd/ZRJkhZu+hMD9N4u83g7\nuLzo1BHLGkCddQKbeKjx4g6FsyxfOCp/A7JP5yudDCBz9KLvXL+j/rXft4kEKzSf\ne1jdWM9TfQKBgQCCTQdHz9wV7Ah1nvWrqaTVYPNSFOIXJuaLDCM4XHUve5UY9nDy\nR3JPLzaoNDaSF970w5qT4+u7ce+t+h/r9gaVto8hwbH6mtAxQDCpxPiCZKMn1JU5\num15IT1s8tf3blBx82YhUW2GBImGV04FiiYaGrLoVlhJv/NuP1NXiaq9dQKBgQCH\niyxOrRaV504eZdqUNVxTaKxgf/+fw5FbTJQprhdEcg4uQzohIpCZLHZlqOl0g0DC\nPZxAu8XKq7r8sp0u/7MIJI6PQ1uT34MmKs+CR/FWxqr8lZcRDz8Eg1Yg+8pdkYRg\nv8PVaUDr2DbCxP+2yxY+PmDj4cASdtuBRiNhofyWgQKBgHAHJNpff4c+vt5Qrw7P\nUzTL0/HHPV9ALOS6Ruqw2jg5ywZ1DMSjAgDP7IDKOyTmk4MuAfC4BDvcB990tqmN\nYu7wWIBpPklAcGZ99XBmLbsnw8rEHczzTESlx69uROctgVfBqbndhzNeCz42dDO3\njHMCH5yeTly7C+NPAiMzFwhe\n-----END PRIVATE KEY-----\n",
+
+    };
+    
+    export default firebaseConfig;
+    
