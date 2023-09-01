560a52ea796bc0c177f58d6c81d3972d34dbca1a
Karthi <sahid@myulogic.com>
2023-09-01 13:01:10 +0530

    added databse module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './infra/mongoose/daatabse.module';
 
 import { AuthModule } from './controller/auth/auth.module';
 import { QuestionModule } from './question/question.module';
@@ -10,13 +11,9 @@ import { MongooseConfigService } from './infra/mongoose/mongoose-config.service'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal:true}),
-MongooseModule.forRootAsync({
-  imports:[ConfigModule],
-useClass:MongooseConfigService
 
-}),
-
+  ConfigModule.forRoot({isGlobal:true}),
+DatabaseModule,
     AuthModule,
     QuestionModule,
   ],
