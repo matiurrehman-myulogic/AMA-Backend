a9c9ac06724fcb2b0c6aaa19ea9b32e384992590
Karthi <sahid@myulogic.com>
2023-09-15 13:01:17 +0530

    aaddec chat endpoint

--- src/controller/chat/chat.controller.ts
+++ src/controller/chat/chat.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import * as mongoose from 'mongoose';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/AuthGuard/authGuard';
+@UseGuards(AuthGuard)
+@ApiBearerAuth('JWT')
+@ApiTags('Chat')
 
 @Controller('chat')
 export class ChatController {
@@ -11,23 +17,23 @@ export class ChatController {
   create(@Body() createChatDto: CreateChatDto) {
       return this.chatService.createRoom(createChatDto);
     }
-  @Get()
-  findAll() {
-    return this.chatService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.chatService.findOne(+id);
+  findChatroom(@Param('id') id:mongoose.Types.ObjectId ) {
+    return this.chatService.findChatroom(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChatDto: UpdateChatDto) {
-    return this.chatService.update(+id, updateChatDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.chatService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.chatService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateChatDto: UpdateChatDto) {
+  //   return this.chatService.update(+id, updateChatDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.chatService.remove(+id);
+  // }
 }
