82217409a6e1c3f727edc8e656cf753c5c1cae04
Karthi <sahid@myulogic.com>
2023-10-03 16:02:40 +0530

    final changes to authguard

--- src/controller/chat/chat.controller.ts
+++ src/controller/chat/chat.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ChatService } from './chat.service';
 import * as mongoose from 'mongoose';
 import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
@@ -7,55 +16,50 @@ import { CreateChatDto } from 'src/DTO/create-chat.dot';
 import { FindChatDto } from 'src/DTO/findchat-dto';
 import { UpdateChatDto } from 'src/DTO/updateChat.dto';
 import { ChatGateway } from './chat.gateway';
-@UseGuards(AuthGuard)
-@ApiBearerAuth('JWT')
-@ApiTags('Chat')
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
-  constructor(private readonly chatService: ChatService,
-    private readonly chatGateway: ChatGateway
-    ) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly chatGateway: ChatGateway,
+  ) {}
 
   @Post('create')
   create(@Body() createChatDto: CreateChatDto) {
-      return this.chatService.createRoom(createChatDto);
-    }
+    return this.chatService.createRoom(createChatDto);
+  }
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('JWT')
   @Get(':id')
   // @ApiParam({ name: 'id', type: mongoose.Types.ObjectId, description: 'The ID of the chatroom' })
-// @Param('id') id: string
+  // @Param('id') id: string
   // findChatroom(@Body() id:FindChatDto) {
   //   return this.chatService.findChatroom(id);
   // }
-  findChatroom(@Param('id') id:string) {
+  findChatroom(@Param('id') id: string) {
     return this.chatService.findChatroom(id);
   }
 
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.chatService.findOne(+id);
-  // }
-
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('JWT')
   @Patch(':id')
-  updafindChatroom(@Param('id') id:string,@Body() updateChatDto:UpdateChatDto) {
-
+  updafindChatroom(
+    @Param('id') id: string,
+    @Body() updateChatDto: UpdateChatDto,
+  ) {
     //     const updatedChat = await this.chatService.updateChatMessage(updateChatDto, id);
 
     // // Send a WebSocket message to inform clients about the updated chat message
     // this.chatGateway.server.emit('chatMessage', updatedChat);
 
     // return updatedChat;
-    
+
     // return this.chatService.updateChatMessage(updateChatDto,id);
 
-
-     
-    return this.chatService.updateChatMessage(updateChatDto,id);
+    return this.chatService.updateChatMessage(updateChatDto, id);
   }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.chatService.remove(+id);
-  // }
+
 }
